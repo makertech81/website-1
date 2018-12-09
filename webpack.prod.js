15779ea38c57744aeec124f1ef8ce5320befe565
Nicholas Yang <ny585@nyu.edu>
2018-12-09 17:11:14 -0500

    reformatted webpack config files

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,10 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
 
-module.exports = merge(common, { mode: "production", output: {
+module.exports = merge(common, {
+  mode: "production",
+  output: {
     path: path.resolve(__dirname, "dist/prod"),
     filename: "build.js"
-  } });
+  }
+});
