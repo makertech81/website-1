bf171a33042c50f174c0f0f2fe5c966e4b42f1ea
Nicholas Yang <ny585@nyu.edu>
2018-12-10 14:32:57 -0500

    removed extra line from webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,6 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
 
-
 module.exports = merge(common, {
   mode: "production",
   output: {
