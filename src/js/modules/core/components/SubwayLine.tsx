762aaff9e992c51480992edfc48cb8e572e0ff4e
Nicholas Yang <ny585@nyu.edu>
2018-12-09 22:32:56 -0500

    Added fancier animation using some basic state and react-jss. Hopefully will be somewhat performant

--- src/js/modules/core/components/SubwayLine.tsx
+++ src/js/modules/core/components/SubwayLine.tsx
@@ -1,67 +1,123 @@
 import * as React from "react";
 import { Styles } from "react-jss";
 import injectSheet from "react-jss/lib/injectSheet";
+import { Component } from "react";
+import { delay, getRandomInteger } from "../../utils";
+import SubwayStop from "./SubwayStop";
+import { STOPS_COUNT } from "../../constants";
 
-interface Props {
+interface SubwayLineProps {
   color: string;
-  delay: string;
+  lineStep: number;
+  stopOffsets: number[];
+  lineOffset: number;
+  currentStop: number;
   classes: { [s: string]: string };
 }
 
 const styles: Styles = {
   SubwayLine: {
     height: "15px",
-    width: "0px",
+    // @ts-ignore
+    width: props => `${props.lineStep * (100 / STOPS_COUNT) + props.lineOffset}vw`,
+    transition: "width 1.5s",
     margin: "10px 0px 10px 0px",
-    borderRadius: "5%",
+    borderRadius: "10%",
     // @ts-ignore
-    backgroundColor: props => props.color,
-    animation: "line 10s infinite",
-    // @ts-ignore
-    animationDelay: props => props.delay
-  },
-  "@keyframes line": {
-    "0%": {
-      width: "0%"
-    },
-    "9%": {
-      width: "25%"
-    },
-    "18%": {
-      width: "50%"
-    },
-    "27%": {
-      width: "75%"
-    },
-    "36%": {
-      width: "100%"
-    },
-    "45%": {
-      width: "100%"
-    },
-    "54%": {
-      width: "100%"
-    },
-    "63%": {
-      width: "100%"
-    },
-    "72%": {
-      width: "75%"
-    },
-    "81%": {
-      width: "50%"
-    },
-    "90%": {
-      width: "25%"
-    },
-    "100%": {
-      width: "0%"
-    }
+    backgroundColor: props => props.color
   }
 };
 
-const SubwayLine: React.SFC<Props> = ({ classes }) => {
-  return <div className={classes.SubwayLine} />;
+const SubwayLine: React.SFC<SubwayLineProps> = ({
+  classes,
+  currentStop,
+  stopOffsets
+}) => {
+  let subwayStops = [];
+  // Wow I think this is my first traditional for loop in JS in ages
+  for (let i = 0; i < STOPS_COUNT; i++) {
+    subwayStops.push(
+      <SubwayStop
+        key={i}
+        stopIndex={i}
+        currentStop={currentStop}
+        offset={stopOffsets[i]}
+      />
+    );
+  }
+  return <div className={classes.SubwayLine}>{subwayStops}</div>;
 };
 
-export default injectSheet(styles)(SubwayLine);
+const StyledSubwayLine = injectSheet(styles)(SubwayLine);
+
+interface State {
+  lineStep: number;
+  currentStop: number;
+  stopOffsets: number[];
+  lineOffset: number;
+}
+
+interface Props {
+  delay: number;
+  color: string;
+}
+
+class SubwayLineWrapper extends Component<Props, State> {
+  isMounted: boolean;
+
+  constructor(props: Props) {
+    super(props);
+    let stopOffsets = [];
+    for (let i = 0; i < STOPS_COUNT; i++) {
+      stopOffsets.push(getRandomInteger(5) + 3);
+    }
+    this.isMounted = true;
+    this.state = {
+      lineStep: 0,
+      currentStop: 0,
+      lineOffset: getRandomInteger(5) + 3,
+      stopOffsets
+    };
+  }
+
+  componentDidMount() {
+    delay(this.props.delay).then(() => this.animate());
+  }
+
+  componentWillUnmount() {
+    this.isMounted = false;
+  }
+
+  async animate() {
+    while (this.isMounted) {
+      while (this.state.lineStep < STOPS_COUNT) {
+        await delay(1000 + getRandomInteger(1000));
+        this.setState({ lineStep: this.state.lineStep + 1 });
+        await delay(1500);
+        this.setState({ currentStop: this.state.currentStop + 1 });
+      }
+      await delay(2000);
+      while (this.state.lineStep > 0) {
+        await delay(1500);
+        this.setState({ currentStop: this.state.currentStop - 1 });
+        await delay(1000 + getRandomInteger(1000));
+        this.setState({ lineStep: this.state.lineStep - 1 });
+      }
+    }
+  }
+
+  render() {
+    const { color } = this.props;
+    const { lineStep, stopOffsets, lineOffset, currentStop } = this.state;
+    return (
+      <StyledSubwayLine
+        lineOffset={lineOffset}
+        color={color}
+        stopOffsets={stopOffsets}
+        lineStep={lineStep}
+        currentStop={currentStop}
+      />
+    );
+  }
+}
+export default SubwayLineWrapper;
---
+++ src/js/modules/core/components/SubwayStop.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { Styles } from "react-jss";
+import injectSheet from "react-jss/lib/injectSheet";
+import { STOPS_COUNT } from "../../constants";
+
+const styles: Styles = {
+  SubwayStop: {
+    backgroundColor: "white",
+    position: "absolute",
+    borderRadius: "10px",
+    transition: "transform 1s",
+    width: "20px",
+    height: "20px",
+    //@ts-ignore
+    left: props =>
+      `${(props.stopIndex + 1) * (100 / STOPS_COUNT) - props.offset}vw`,
+    //@ts-ignore
+    transform: props =>
+      props.stopIndex < props.currentStop ? "scale(1, 1)" : "scale(0, 0)"
+  }
+};
+
+interface Props {
+  classes: { [s: string]: string };
+  currentStop: number;
+  stopIndex: number;
+  offset: number;
+}
+
+const SubwayStop: React.SFC<Props> = ({ classes }) => {
+  return <div className={classes.SubwayStop} />;
+};
+
+export default injectSheet(styles)(SubwayStop);
