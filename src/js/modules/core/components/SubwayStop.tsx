69aa59e5d92af2bff254f4963ce23a50dd608841
Nicholas Yang <ny585@nyu.edu>
2018-12-16 19:56:51 -0500

    refactored SubwayStop

--- src/js/modules/core/components/SubwayStop.tsx
+++ src/js/modules/core/components/SubwayStop.tsx
@@ -2,8 +2,20 @@ import * as React from "react";
 import { Styles } from "react-jss";
 import injectSheet from "react-jss/lib/injectSheet";
 import { STOPS_COUNT } from "../../constants";
+import { JssRules } from "../../types";
 
-const styles: Styles = {
+interface SubwayStopStyles<T> extends Styles {
+  SubwayStop: T
+}
+
+interface Props {
+  classes: SubwayStopStyles<string>
+  currentStop: number;
+  stopIndex: number;
+  offset: number;
+}
+
+const styles: SubwayStopStyles<JssRules> = {
   SubwayStop: {
     backgroundColor: "white",
     position: "absolute",
@@ -12,21 +24,13 @@ const styles: Styles = {
     width: "25px",
     height: "25px",
     border: "0.5px solid #d9d9d9",
-    //@ts-ignore
     left: props =>
       `${(props.stopIndex + 1) * (100 / STOPS_COUNT) - props.offset}vw`,
-    //@ts-ignore
     transform: props =>
       props.stopIndex < props.currentStop ? "scale(1, 1) translateY(-6px)" : "scale(0, 0) translateY(-6px)"
   }
 };
 
-interface Props {
-  classes: { [s: string]: string };
-  currentStop: number;
-  stopIndex: number;
-  offset: number;
-}
 
 const SubwayStop: React.SFC<Props> = ({ classes }) => {
   return <div className={classes.SubwayStop} />;
