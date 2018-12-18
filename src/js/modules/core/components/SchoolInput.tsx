0dc9f41670c5a88d1c4ae7a940b064c00b4cbe24
Michael Yang <themichaelyang@gmail.com>
2018-12-17 17:56:30 -0800

    Convert school input to stateless component

--- src/js/modules/core/components/SchoolInput.tsx
+++ src/js/modules/core/components/SchoolInput.tsx
@@ -6,60 +6,46 @@ interface InputState<T> {
 }
 
 const isUpper = (str) => str === str.toUpperCase()
+const doesStringMatch = (item, value) => {
+  if (value !== '') {
+    if (isUpper(value)) {
+      const firstLetters = item.split(" ").map(word => word[0]);
 
-export default class SchoolInput extends React.Component<Props, InputState<string>> {
-  schools: Array<string>;
-  props: Props;
-
-  constructor(props: Props) {
-    super(props);
-    this.schools = props.schools;
-    this.state = { value: '' };
-    this.props = props;
-    console.log(this.props);
-  }
-
-  doesStringMatch(item, value) {
-    if (value !== '') {
-      if (isUpper(value)) {
-        const firstLetters = item.split(" ").map(word => word[0]);
-
-        for (let char of [...value]) {
-          if (!firstLetters.includes(char)) {
-            return false;
-          }
+      for (let char of [...value]) {
+        if (!firstLetters.includes(char)) {
+          return false;
         }
+      }
 
-        return true;
-      }
-      else {
-        return item.toLowerCase().indexOf(value.toLowerCase()) !== -1;
-      }
+      return true;
     }
     else {
-      return false;
+      return item.toLowerCase().indexOf(value.toLowerCase()) !== -1;
     }
   }
-
-  render() {
-    const { input } = this.props;
-
-    return (
-      <Autocomplete
-        {...input}
-        getItemValue={(item) => item}
-        items={ this.schools }
-        shouldItemRender={ this.doesStringMatch }
-        renderItem={(item, isHighlighted) =>
-          <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
-            { item }
-          </div>
-        }
-        // value={ this.state.value }
-        onChange={ (val) => input.onChange(val) }
-        onSelect={ (val) => input.onChange(val) }
-        // onSelect={(value) => this.setState({ value })}
-      />
-    );    
+  else {
+    return false;
   }
+}
+
+const SchoolInput: React.SFC<Props> = (props) => {
+  const { schools, input } = props;
+
+  return (
+    <Autocomplete
+      {...input}
+      getItemValue={(item) => item}
+      items={ schools }
+      shouldItemRender={ doesStringMatch }
+      renderItem={(item, isHighlighted) =>
+        <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+          { item }
+        </div>
+      }
+      onChange={ (val) => input.onChange(val) }
+      onSelect={ (val) => input.onChange(val) }
+    />
+  );    
 };
+
+export default SchoolInput;
