9d55f604a41664f96ec8f5f3b226273dcf030081
Michael Yang <themichaelyang@gmail.com>
2018-12-17 18:54:00 -0800

    Fix school input field styles

--- src/js/modules/core/components/SchoolInput.tsx
+++ src/js/modules/core/components/SchoolInput.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import { Theme } from "../../types";
+import injectSheet from "react-jss/lib/injectSheet";
+
 import Autocomplete from "react-autocomplete";
 
 interface InputState<T> {
@@ -29,29 +32,43 @@ const doesStringMatch = (item, value) => {
   }
 }
 
+const schoolInputStyles = (theme: Theme): Styles => ({
+  input: {
+    fontFamily: theme.fontFamily,
+    marginLeft: "5px",
+    padding: "5px",
+    fontSize: "1em",
+    position: "relative"
+  }
+});
+
 const SchoolInput: React.SFC<Props> = (props) => {
-  const { schools, input } = props;
+  const { schools, input, classes } = props;
   const menuStyle = {
     borderRadius: '3px',
     boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
     background: 'rgba(255, 255, 255, 0.9)',
     padding: '2px 0',
     fontSize: '90%',
-    top:  '35px', // height of your input
+    top: '50px', // height of your input
     left: 0,
     position: 'absolute',
     overflow: 'auto',
     width: '100%',
     zIndex: 1000
   };
+
   const wrapperStyle = {
     display: 'inline-block',
     position: 'relative'
   };
 
+  console.log(props);
+
   return (
     <Autocomplete
       {...input}
+      renderInput={(props) => <input {...props} className={classes.input} />}
       getItemValue={(item) => item}
       items={ schools }
       shouldItemRender={ doesStringMatch }
@@ -68,4 +85,5 @@ const SchoolInput: React.SFC<Props> = (props) => {
   );    
 };
 
-export default SchoolInput;
+// adds `classes` to props
+export default injectSheet(schoolInputStyles)(SchoolInput);
