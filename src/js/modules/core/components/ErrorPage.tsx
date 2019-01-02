3e07b5323c221246e5253a406d7bb0089307b253
Nicholas Yang <ny585@nyu.edu>
2019-01-02 14:18:01 -0500

    Added animated subway lines to error page

--- src/js/modules/core/components/ErrorPage.tsx
+++ src/js/modules/core/components/ErrorPage.tsx
@@ -2,10 +2,12 @@ import * as React from "react";
 import { Styles } from "react-jss";
 import { JssRules, Theme } from "../../types";
 import injectSheet from "react-jss/lib/injectSheet";
+import AnimatedSubwayLines from "./AnimatedSubwayLines";
 
 interface ErrorPageStyles<T> extends Styles {
   ErrorPage: T;
   message: T;
+  link: T;
 }
 
 interface Props {
@@ -17,26 +19,34 @@ const styles = (theme: Theme): ErrorPageStyles<JssRules> => ({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    padding: "5%",
     fontSize: "1.3em",
+    color: theme.fontColor,
     fontFamily: theme.fontFamily,
     width: "80vw"
   },
   message: {
+    padding: "5%",
     maxWidth: "600px"
+  },
+  link: {
+    color: theme.fontColor,
+    '&:hover': {
+      textDecoration: "underline"
+    }
   }
-})
+});
 
 const ErrorPage: React.SFC<Props> = ({ classes }) => {
   return (
     <div className={classes.ErrorPage}>
       <div className={classes.message}>
-      We're having trouble fetching your data due to <del>
-        train traffic{" "}
-      </del>
-      network problems. Sorry!
+        We're having trouble fetching your data due to <del>train traffic</del>{" "}
+        network problems. Sorry!
       </div>
-      <a href="/"> Click here to try again </a>
+      <a href="/">
+        <div className={classes.link}> Click here to try again </div>
+      </a>
+      <AnimatedSubwayLines />
     </div>
   );
 };
