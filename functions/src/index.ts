0de0c3418b33b4e8d79b564278882deb533fbaf0
Nicholas Yang <ny585@nyu.edu>
2019-01-02 00:43:39 -0500

    Emailing kinda works

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,25 +10,32 @@ admin.initializeApp(functions.config().firebase);
 export const sendConfirmationEmail = functions.firestore
   .document("users/{userId}")
   .onWrite((change, context) => {
-    console.log("CHANGES");
-    console.log(change.after.data());
-    console.log(change.before.data());
-    const timestampInAfter = "submitTimestamp" in change.after.data();
-    const timestampNotInBefore = !("submitTimestamp" in change.before.data());
-    if (timestampInAfter && timestampNotInBefore) {
-      const apiKey = functions.config().sendgrid.key;
-      sgMail.setApiKey(apiKey);
-      const msg = {
-        to: context.auth.token.email,
-        from: "support.hack@nyu.edu",
-        subject: "Confirming Your Submission!",
-        text:
-          "Congratulations on submitting your application for HackNYU 2019! We hope to see you there!",
-        html:
-          "Congratulations on submitting your application for <strong> HackNYU 2019! " +
-          "</strong> We hope to see you there!"
-      };
-      return sgMail.send(msg).then(() => ).catch(err => console.error(err));
-    }
+    return admin
+      .auth()
+      .getUser(context.params.userId)
+      .then(user => {
+        const beforeData = change.before.data();
+        const afterData = change.after.data();
+        const timestampInAfter = afterData && "submitTimestamp" in afterData;
+        const timestampInBefore = beforeData && "submitTimestamp" in beforeData;
+        if (timestampInAfter && !timestampInBefore) {
+          const apiKey = functions.config().sendgrid.key;
+          sgMail.setApiKey(apiKey);
+          const msg = {
+            to: user.email,
+            from: "confirm@hacknyu.org",
+            subject: "Confirming Your Submission!",
+            text:
+              "Congratulations on submitting your application for HackNYU 2019! We hope to see you there!",
+            html:
+              "Congratulations on submitting your application for <strong> HackNYU 2019! " +
+              "</strong> We hope to see you there!"
+          };
+          return sgMail.send(msg);
+        }
+        return undefined;
+      })
+      .then(() => console.log("SENT!"))
+      .catch(err => console.error(err));
   });
 
