ececf99a9db3b944075d124baa640fde3e35d3ca
Nicholas Yang <ny585@nyu.edu>
2019-01-18 18:40:46 +0100

    added redux thunk typings

--- src/js/modules/core/components/UploadResumeButton.tsx
+++ src/js/modules/core/components/UploadResumeButton.tsx
@@ -3,12 +3,11 @@ import Button from "./Button";
 import injectSheet, { WithStyles } from "react-jss";
 import { bindActionCreators, compose, Dispatch } from "redux";
 import { connect } from "react-redux";
-import { uploadResume } from "../coreActions"
+import { uploadResume } from "../coreActions";
 import { Theme } from "../../ThemeInjector";
 import { ReduxState } from "../../types";
 
-
-interface Props extends WithStyles<typeof styles>{
+interface Props extends WithStyles<typeof styles> {
   resumeTimestamp: string;
   uploadResume: (uid: string, file: File) => any;
   uid: string;
@@ -20,7 +19,7 @@ const styles = (theme: Theme) => ({
     flexDirection: "row",
     fontSize: "1.3rem",
     padding: "15px",
-    alignItems: "center",
+    alignItems: "center"
   },
   label: {
     width: "175px",
@@ -40,11 +39,7 @@ const styles = (theme: Theme) => ({
 });
 
 class UploadResumeButton extends React.Component<Props> {
-  private fileUploader = React.createRef<HTMLInputElement>()
-
-  constructor(props: Props) {
-    super(props);
-  }
+  private fileUploader = React.createRef<HTMLInputElement>();
 
   handleClick = (event: Event) => {
     event.preventDefault();
@@ -57,31 +52,46 @@ class UploadResumeButton extends React.Component<Props> {
     const { uid, uploadResume } = this.props;
     const file = this.fileUploader.current.files[0];
     uploadResume(uid, file);
-  }
+  };
 
   render() {
     let { classes, resumeTimestamp } = this.props;
 
-    return <div className={classes.UploadResumeButton}>
-      <div className={classes.label}>
-      (Optional) Resume:
+    return (
+      <div className={classes.UploadResumeButton}>
+        <div className={classes.label}>(Optional) Resume:</div>
+        <input
+          key={0}
+          type="file"
+          className={classes.hiddenInput}
+          ref={this.fileUploader}
+          onChange={this.handleUpload}
+        />
+        <Button key={1} type="button" onClick={this.handleClick}>
+          Upload
+        </Button>
+        {resumeTimestamp && (
+          <div className={classes.uploadedTime}>
+            {" "}
+            Uploaded at {resumeTimestamp}{" "}
+          </div>
+        )}
       </div>
-      <input
-        key={0}
-        type="file"
-        className={classes.hiddenInput}
-        ref={this.fileUploader}
-        onChange={this.handleUpload}
-      />
-      <Button key={1} type="button" onClick={this.handleClick}>Upload</Button>
-      {resumeTimestamp  && <div className={classes.uploadedTime}> Uploaded at {resumeTimestamp} </div> }
-    </div>
+    );
   }
 }
 
-const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators({ uploadResume }, dispatch)
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators({ uploadResume }, dispatch);
+
 const mapStateToProps = (state: ReduxState) => ({
   resumeTimestamp: state.core.applyForm.resumeTimestamp
-})
+});
 
-export default compose(injectSheet(styles), connect(mapStateToProps, mapDispatchToProps))(UploadResumeButton);
+export default compose(
+  injectSheet(styles),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+)(UploadResumeButton);
