039676ca5f7efd178db711b8ba1981db2cfa111e
Michael Yang <themichaelyang@gmail.com>
2019-02-01 00:20:02 -0500

    Add loading of scanned user data (WIP)

--- src/js/modules/core/components/UserCheckInPage.tsx
+++ src/js/modules/core/components/UserCheckInPage.tsx
@@ -6,6 +6,7 @@ import { ReduxState } from "../../../reducers";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { RouteComponentProps } from "react-router-dom";
+import { db } from "../../../firebase";
 
 type PathParams = {
   uid: string 
@@ -26,16 +27,62 @@ const styles = (theme: Theme) => ({
   },
   title: {
     marginBottom: "1em"
+  },
+  infoLine: {
+    display: "block"
   }
 });
 
-const UserCheckInPage: React.SFC<Props & RouteComponentProps<PathParams>> = ({ classes, match }) => {
-  console.log(match);
-  return (
-    <div className={classes.UserCheckInPage}>
-      <h1 className={classes.title}> UID: {match.params.uid} </h1>
-    </div>
-  );
+async function fetchUserData(uid: string): Promise<{ userData: any, isAdmitted: boolean }> {
+  let userData = {};
+  let isAdmitted: boolean = false;
+
+  userData = await db.collection("users").doc(uid).get().then((doc: any) => doc.data());
+  isAdmitted = await db.collection("users").doc(uid).get().then((doc: any) => doc.exists);
+  
+  return { userData, isAdmitted };
+}
+
+// This is in a terrible state but please forgive me!
+class UserCheckInPage extends React.Component<Props & RouteComponentProps<PathParams>> {
+  state: { userData: any, isAdmitted: boolean } = {
+    userData: { firstName: '', lastName: '' },
+    isAdmitted: false
+  };
+  _asyncRequest = null;
+
+  constructor(props) {
+    super(props);
+  } 
+
+  componentWillMount() {
+    this._asyncRequest = fetchUserData(this.props.match.params.uid).then(({ userData, isAdmitted }) => {
+      this._asyncRequest = null;
+      this.setState({ userData, isAdmitted });
+    });
+  }
+
+  componentWillUnmount() {
+    if (this._asyncRequest) {
+      this._asyncRequest.cancel();
+    }
+  }
+
+  render() {
+    const { classes, match } = this.props;
+    const { userData, isAdmitted } = this.state;
+
+    return (
+      <div className={classes.UserCheckInPage}>
+        <h1 className={classes.title}> UID: {match.params.uid} </h1>
+        <div>
+          <span className={classes.infoLine}> First name: {userData.firstName} </span>
+          <span className={classes.infoLine}> Last name: {userData.lastName} </span>
+        </div>
+        
+      </div>
+    );
+  }
 };
 
 export default injectSheet(styles)(UserCheckInPage);
